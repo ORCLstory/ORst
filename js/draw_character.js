077787939042074e160c8028d0954b4d487bdeb9
motto <s19361@s.kaetsu.ac.jp>
2020-03-22 12:30:58 +0900

    pointsをthis.pointsニ変えた

--- js/draw_character.js
+++ js/draw_character.js
@@ -6,19 +6,19 @@ class DrawCharacter{
     enemy(enemyList){
         const dcp = new DrawCharacterProperty();
         character_context.clearRect(0, 0, character_canvas.width, character_canvas.height);
-        let points = dcp.drawEnemyCoordinate(enemyList.length);
+        this.points = dcp.drawEnemyCoordinate(enemyList.length);
         for(let i = 0; i < enemyList.length; i++){
-            createCircle(points[i]["x"],points[i]["y"], 20);
+            createCircle(this.points[i]["x"],this.points[i]["y"], 20);
         }
     }
 
     ally(allyList){
         console.log(allyList);
         const dcp = new DrawCharacterProperty();
-        let points = dcp.drawAllyCoordinate(allyList.length);
-        console.log(points);
+        this.points = dcp.drawAllyCoordinate(allyList.length);
+        console.log(this.points);
         for(let i = 0; i < allyList.length; i++){
-            createCircle(points[i]["x"],points[i]["y"], 15);
+            createCircle(this.points[i]["x"],this.points[i]["y"], 15);
         }
     }
 }
