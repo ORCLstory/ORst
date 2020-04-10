ea666b2af4fea82a667b8a36a9075b17b1b2a92a
RyoyaMizukami <s18102@kaetsu.ac.jp>
2020-04-10 11:43:33 +0900

    リファクタリング途中交代

--- js/Models/battle_system.js
+++ js/Models/battle_system.js
@@ -19,19 +19,18 @@ async function startBattleSystem(){
     enemyList.push(slime2);
     enemyList.push(slime3);
 
-    let results = [];
+    let promise_results = [];
     for(let i = 0; i < allyList.length; i++){
         // AllyStatus.setStatusの引数はレベル
-        results.push(allyList[i].setStatus(1));
+        promise_results.push(allyList[i].setStatus(1));
     }
 
     for(let i = 0; i < enemyList.length; i++){
-        results.push(enemyList[i].setStatus(1));
+        promise_results.push(enemyList[i].setStatus(1));
     }
-    results.push(magic_list.setAllMagicList());
+    promise_results.push(magic_list.setAllMagicList());
     // スプレッドシートから情報を非同期で取得するため、全部ステータスを取得するまで待つ
-    await Promise.all(results);
-
+    await Promise.all(promise_results);
 
     console.log(allyList);
 
@@ -40,6 +39,8 @@ async function startBattleSystem(){
 }
 
 function* battleSystem(){
+    // BattleSystemクラスを呼び出し
+    system = new BattleSystem;
 
     // 味方の情報をリストに格納
     showStatus(allyList);
@@ -51,81 +52,57 @@ function* battleSystem(){
     drawFightScene();
     battlelog.encount();
 
-    //敵と味方を出す
-    g_draw_character_instance = new DrawCharacter();
-    g_draw_character_instance.enemy(enemyList);
-    g_draw_character_instance.ally(allyList);
+    //敵と味方を描画する
+    system.drawAllCharacter(enemyList, allyList);
 
+    // 1ターンの処理
     while (true){
+        // 行動できる敵・味方を更新
+        let actionableAllyList = system.refreshActionableList(allyList, enemyList)[0];
+        let actionableEnemyList = system.refreshActionableList(allyList, enemyList)[1];
+
+        // どちらかが全滅しているかどうかを確認
+        system.isWipe(actionableAllyList, actionableEnemyList);
+        yield 0;
+
+        // 各キャラクターの行動を初期化
         let commandQueue = [];
 
-        // 行動可能な味方をリストに格納
-        let actionableAllyList = allyList.filter(target => target.status.some(status => status === 'alive'));
-        // 行動可能な敵をリストに格納
-        let actionableEnemyList = enemyList.filter(target => target.status.some(status => status === 'alive'));
-
-        // 全滅しているかどうかを判定
-        if (actionableAllyList.length === 0){
-            battlelog.defeat();
-            cursor.current_cursor = 'end';
-            yield 0;
-        }
-        else if (actionableEnemyList.length === 0){
-            battlelog.victory();
-            cursor.current_cursor = 'end';
-            yield 0;
-        }
-
+        // 行動選択をするキャラクターを先頭に戻す
         cursor.current_select_character = 0;
-        // 味方が攻撃対象を選択
+
+        // 全員のコマンド選択が終わるまでループさせる
         while (true){
+            // 「◯◯はどうする？」というメッセージを表示
             battlelog.decision(actionableAllyList[cursor.current_select_character]);
+            // コマンド選択で使う数字を定義
             let current_select_character = cursor.current_select_character;
             yield 0;
-            // もし増えてたら決定してます。
-            if (current_select_character < cursor.current_select_character){
-                if (cursor.current_command_number === 1){
-                    commandQueue.push({'player':actionableAllyList[current_select_character],'target':enemyList[cursor.choice_current_enemy],'action':magic_list.allMagicList[cursor.current_magic_cursor()]});
-                }
-                else {
-                    commandQueue.push({'player':actionableAllyList[current_select_character],'target':enemyList[cursor.choice_current_enemy],'action':null});
-                }
 
-                console.log(cursor.current_command_number);
-                if (commandQueue[commandQueue.length - 1].action === null){
-                    console.log('戦うを選択したよ');
-                }
-                else if (commandQueue[commandQueue.length - 1].action instanceof Magic){
-                    console.log('魔法を選択したよ');
-                    console.log(magic_list.allMagicList[cursor.current_magic_cursor()].name);
-                }
-            }
-            // もし減っていたらキャンセルしたとみなします。
-            else if ( current_select_character > cursor.current_select_character) {
-                commandQueue.pop();
-            }
+            // 味方の行動を選択
+            system.decisionCommand(current_select_character, commandQueue, actionableAllyList, enemyList);
+
+            // 全員のコマンドが選択し終わったら次の処理へ
             if (cursor.current_select_character >= actionableAllyList.length){
-                console.log(cursor.current_select_character);
                 break;
             }
-            // cursorの位置を初期化して、次のキャラクターに行動させます
-            cursor.initialize_when_choice_next_character();
-        }
-        console.log(commandQueue);
 
-        // 敵AIの攻撃対象選択
-        for (let i = 0; i < actionableEnemyList.length; i++){
-            commandQueue.push({'player':actionableEnemyList[i],'target':actionableAllyList[selectTarget(actionableAllyList)], 'action':null});
+            // cursorの位置を初期化して、次のキャラクターに行動させる
+            cursor.initialize_when_choice_next_character();
         }
 
         // コマンド入力終了時の処理
         gc_context.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
         cursor.current_cursor = 'read_message';
-        // battle_logを初期化
-        battle_log_list = [];
+
+        // 敵AIの攻撃対象選択
+        system.enemySelectTarget(actionableEnemyList, actionableAllyList, commandQueue);
+
+        // commandQueueを素早さ順で並び替え
+
         // commandQueueに追加された行動を順番に処理していく
         for (let i = 0; i < commandQueue.length; i++){
-            // プレイヤーが行動不能な場合、処理を行わず次のプレイヤーに行動させる
+            // 攻撃側が行動不能な場合、処理を行わず次のプレイヤーに行動させる
             if (commandQueue[i].player.status.some(status => status === 'dead')){
                 continue;
             // 対象が選択不能な場合、対象を変える
@@ -227,6 +204,70 @@ function enemyNumbering(enemyList){
         }
         if (duplicationCounter > 0){
             enemyList[i].individual_name = enemyList[i].name + String.fromCharCode(65);
+        }
+    }
+}
+
+class BattleSystem{
+    constructor(){
+    }
+
+    drawAllCharacter(enemyList, allyList){
+        g_draw_character_instance = new DrawCharacter();
+        g_draw_character_instance.enemy(enemyList);
+        g_draw_character_instance.ally(allyList);
+    }
+
+    refreshActionableList(allyList, enemyList){
+        // 行動可能な味方をリストに格納
+        let actionableAllyList = allyList.filter(target => target.status.some(status => status === 'alive'));
+        // 行動可能な敵をリストに格納
+        let actionableEnemyList = enemyList.filter(target => target.status.some(status => status === 'alive'));
+        let actionableList = [actionableAllyList, actionableEnemyList];
+
+        return actionableList;
+    }
+
+    isWipe(actionableAllyList, actionableEnemyList){
+        // 全滅しているかどうかを判定
+        if (actionableAllyList.length === 0){
+            battlelog.defeat();
+            cursor.current_cursor = 'end';
+        }
+        else if (actionableEnemyList.length === 0){
+            battlelog.victory();
+            cursor.current_cursor = 'end';
+        }
+    }
+
+    decisionCommand(current_select_character, commandQueue, actionableAllyList, enemyList){
+        // もし増えてたら決定してます。
+        if (current_select_character < cursor.current_select_character){
+            if (cursor.current_command_number === 1){
+                commandQueue.push({'player':actionableAllyList[current_select_character],'target':enemyList[cursor.choice_current_enemy],'action':magic_list.allMagicList[cursor.current_magic_cursor()]});
+            }
+            else {
+                commandQueue.push({'player':actionableAllyList[current_select_character],'target':enemyList[cursor.choice_current_enemy],'action':null});
+            }
+
+            console.log(cursor.current_command_number);
+            if (commandQueue[commandQueue.length - 1].action === null){
+                console.log('戦うを選択したよ');
+            }
+            else if (commandQueue[commandQueue.length - 1].action instanceof Magic){
+                console.log('魔法を選択したよ');
+                console.log(magic_list.allMagicList[cursor.current_magic_cursor()].name);
+            }
+        }
+        // もし減っていたらキャンセルしたとみなします。
+        else if (current_select_character > cursor.current_select_character) {
+            commandQueue.pop();
+        }
+    }
+
+    enemySelectTarget(actionableEnemyList, actionableAllyList, commandQueue){
+        for (let i = 0; i < actionableEnemyList.length; i++){
+            commandQueue.push({'player':actionableEnemyList[i],'target':actionableAllyList[selectTarget(actionableAllyList)], 'action':null});
         }
     }
 }
