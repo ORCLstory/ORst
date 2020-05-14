f080b686f7462194b90031fa6e7868bc1df50fe7
K1-scuba_diving <tellurium38@gmail.com>
2020-05-14 11:58:36 +0900

    favicon.icoを設定

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ app.debug = True
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'img/teo_kao.png'))
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(port=port)
